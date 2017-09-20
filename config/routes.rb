40999d60b92009196cc9b5b3fc94d413ccd179e4
Chris Fowke <Cfowke@Chriss-MacBook-Pro.local>
2017-09-20 17:41:40 -0500

    account creationfeature all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # :controllers => { :registrations => :registrations }
 
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
 
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
 end
--- spec/features/Users/create_account_spec.rb
+++ spec/features/Users/create_account_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 feature 'user creates new account' do
 
   let!(:user1) { FactoryGirl.create(:user) }
+  let!(:new_user) {
+    User.new(
+      username: "testuser2",
+      first_name: "Rick",
+      last_name: "Deckard",
+      email: "testuser2@gmail.com",
+      password: "Changeme"
+    )
+  }
 
   scenario 'unauthenticated user visits homepage' do
     visit new_user_session_path
@@ -15,12 +24,12 @@ feature 'user creates new account' do
 
     click_button 'Sign up'
 
-    fill_in 'Username', with: "testuser2"
-    fill_in 'First name', with: "Rick"
-    fill_in 'Last name', with: "Deckard"
-    fill_in 'Email', with: "testuser2@gmail.com"
-    fill_in 'Password', with: "Changeme"
-    fill_in 'Password confirmation', with: "Changeme"
+    fill_in 'Username', with: new_user.username
+    fill_in 'First name', with: new_user.first_name
+    fill_in 'Last name', with: new_user.last_name
+    fill_in 'Email', with: new_user.email
+    fill_in 'Password', with: new_user.password
+    fill_in 'Password confirmation', with: new_user.password
 
     click_button 'Sign up'
 
@@ -32,8 +41,10 @@ feature 'user creates new account' do
 
     click_button 'Sign up'
 
-    fill_in 'Username', with: user1.username
-    fill_in 'Email', with: user1.email
+    fill_in 'Username', with: new_user.username
+    fill_in 'First name', with: new_user.first_name
+    fill_in 'Last name', with: new_user.last_name
+    fill_in 'Email', with: new_user.email
 
     click_button 'Sign up'
 
