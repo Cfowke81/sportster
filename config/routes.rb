45ceb1a695fee746fa3c8399f11e340307a127fc
Chris Fowke <Cfowke@Chriss-MacBook-Pro.local>
2017-09-25 11:49:14 -0500

    user profile page foundation framework setup wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root to: 'devise/sessions#new'
+    root to: 'events#show'
   end
 
   resources :users, only: [:index, :show, :edit, :update]
+  resources :events, only: [:show]
 end
